9a5048925943473dee7bf863b44ea9601cc38b17
TechWZ <38968184+TechWZ@users.noreply.github.com>
2025-06-18 09:24:03 +0800

    Delete server/src/main/java/institute/service/NoteService.java

--- server/src/main/java/institute/service/NoteService.java
+++
@@ -1,8 +0,0 @@
-package institute.service;
-
-import java.util.Map;
-
-public interface NoteService {
-    // 递归列出所有文件
-    Map<Object, Object> listFilesRecursively(String path, Map<Object, Object> directory);
-}
